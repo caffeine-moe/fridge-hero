568eb73bb6d2c2251ab377aa4f7927e41636e7a8
caffeine01 <caffeine@caffeine.moe>
2024-10-24 20:33:47 +0100

    Switch to new TOML notation for package versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 dependencies {
 
     //mongodb realm
-    implementation("io.realm.kotlin:library-base:3.0.0")
+    implementation(libs.library.base)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
