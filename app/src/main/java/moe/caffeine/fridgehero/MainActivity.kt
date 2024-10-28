98e0101a3840a2648252125c6942fa28b0494457
caffeine01 <caffeine@caffeine.moe>
2024-10-28 02:08:11 +0000

    OOBE Screen and logic

--- app/src/main/java/moe/caffeine/fridgehero/MainActivity.kt
+++ app/src/main/java/moe/caffeine/fridgehero/MainActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import moe.caffeine.fridgehero.model.Profile
+import moe.caffeine.fridgehero.oobe.OOBE
+import moe.caffeine.fridgehero.repo.MongoRealm
 import moe.caffeine.fridgehero.ui.theme.FridgeHeroTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val realm = MongoRealm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FridgeHeroTheme {
-
+                realm.fetchAllByType<Profile>().ifEmpty {
+                    OOBE()
+                }
             }
         }
     }
---
+++ app/src/main/java/moe/caffeine/fridgehero/oobe/OOBE.kt
@@ -0,0 +1,75 @@
+package moe.caffeine.fridgehero.oobe
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import moe.caffeine.fridgehero.ui.theme.Typography
+
+@Composable
+fun OOBE() {
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            //BBWM LETS GO
+            Text(
+                style = Typography.headlineMedium,
+                text = "Welcome to Fridge Hero!",
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                modifier = Modifier.padding(10.dp),
+                style = Typography.bodyLarge,
+                text = "Create a profile to get started.",
+                textAlign = TextAlign.Center
+            )
+
+            OutlinedTextField(
+                value = firstName,
+                onValueChange = {
+                    firstName = it.trim().replaceFirstChar { char -> char.titlecase() }
+                },
+                label = { Text("First Name") },
+                placeholder = { Text("John") }
+            )
+
+            OutlinedTextField(
+                value = lastName,
+                onValueChange = {
+                    lastName = it.trim().replaceFirstChar { char -> char.titlecase() }
+                },
+                label = { Text("Last Name") },
+                placeholder = { Text("Doe") }
+            )
+
+
+            Button(
+                modifier = Modifier.padding(10.dp),
+                onClick = {
+                    println("$firstName $lastName")
+                }
+            ) {
+                Text("Create Profile")
+            }
+        }
+    }
+}
