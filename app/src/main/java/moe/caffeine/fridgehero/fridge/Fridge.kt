0a5da8ef62f844f092d62adf1a7ff8038b50bd4b
caffeine01 <caffeine@caffeine.moe>
2024-10-29 22:38:06 +0000

    Add floating action button

--- app/src/main/java/moe/caffeine/fridgehero/fridge/Fridge.kt
+++ app/src/main/java/moe/caffeine/fridgehero/fridge/Fridge.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SwipeToDismissBox
@@ -34,7 +36,13 @@ import androidx.compose.ui.unit.dp
 fun Fridge() {
     val foodItems: MutableList<String> = mutableListOf()
     (0..10).map { foodItems += "ONION" }
-    Scaffold { innerPadding ->
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(Icons.Filled.Add, "Add Item Button")
+            }
+        }
+    ) { innerPadding ->
         LazyColumn(
             contentPadding = innerPadding
         ) {
