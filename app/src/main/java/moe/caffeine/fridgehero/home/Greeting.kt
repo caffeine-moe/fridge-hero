650aee575ffdcbb3d36c30c9f50849c295640eb7
caffeine01 <caffeine@caffeine.moe>
2024-10-29 19:15:20 +0000

    Add sample "expiring soon" and "recipes" cards to home screen

--- app/src/main/java/moe/caffeine/fridgehero/home/Greeting.kt
+++ app/src/main/java/moe/caffeine/fridgehero/home/Greeting.kt
@@ -1,17 +1,13 @@
 package moe.caffeine.fridgehero.home
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun Greeting(name: String) {
     val greetings = listOf("Hey", "Hiya", "Sup", "Hello")
     Text(
-        modifier = Modifier.padding(10.dp),
         text = "${greetings.random()}, ${name}.",
         style = typography.titleLarge
     )
--- app/src/main/java/moe/caffeine/fridgehero/home/Home.kt
+++ app/src/main/java/moe/caffeine/fridgehero/home/Home.kt
@@ -2,19 +2,25 @@ package moe.caffeine.fridgehero.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import moe.caffeine.fridgehero.model.Profile
 
 @Composable
 fun Home(profile: Profile) {
     Surface {
         Column(
+            modifier = Modifier.padding(10.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start
         ) {
             Greeting(profile.firstName)
+            ExpiringSoon()
+            AvailableRecipes()
         }
     }
 }
