0e23ee9ae97b7e9a63e4fcffaf3732f71e68ee6c
caffeine01 <caffeine@caffeine.moe>
2024-10-30 02:32:48 +0000

    add test image to ItemCard

--- app/src/main/java/moe/caffeine/fridgehero/fridge/ItemCard.kt
+++ app/src/main/java/moe/caffeine/fridgehero/fridge/ItemCard.kt
@@ -1,15 +1,22 @@
 package moe.caffeine.fridgehero.fridge
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import moe.caffeine.fridgehero.R
 
 @Composable
 fun ItemCard(item: String) {
@@ -24,6 +31,16 @@ fun ItemCard(item: String) {
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
+            Image(
+                modifier = Modifier
+                    .size(80.dp, 80.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+                painter = painterResource(R.drawable.ic_launcher_background),
+                alignment = Alignment.CenterStart,
+                contentDescription = "",
+                contentScale = ContentScale.Crop
+            )
+            Spacer(Modifier.padding(10.dp))
             Text(item)
         }
     }
