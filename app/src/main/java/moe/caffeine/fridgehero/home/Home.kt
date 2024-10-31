a4e05ea5478e723cf82e6c641cf20b2bf2fc069b
caffeine01 <caffeine@caffeine.moe>
2024-10-31 01:57:52 +0000

    make home screen scrollable

--- app/src/main/java/moe/caffeine/fridgehero/home/Home.kt
+++ app/src/main/java/moe/caffeine/fridgehero/home/Home.kt
@@ -2,7 +2,10 @@ package moe.caffeine.fridgehero.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -11,8 +14,12 @@ import moe.caffeine.fridgehero.model.Profile
 
 @Composable
 fun Home(profile: Profile) {
+    val scrollState = rememberScrollState()
     Column(
-        modifier = Modifier.padding(10.dp),
+        modifier = Modifier
+            .padding(10.dp)
+            .verticalScroll(scrollState)
+            .fillMaxSize(),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start
     ) {
