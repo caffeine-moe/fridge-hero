33384b981353815e56e63dc63b8e75a8d75ef5ff
caffeine01 <caffeine@caffeine.moe>
2024-11-01 16:03:08 +0000

    concurrent food items flow

--- app/src/main/java/moe/caffeine/fridgehero/MainViewModel.kt
+++ app/src/main/java/moe/caffeine/fridgehero/MainViewModel.kt
@@ -1,8 +1,20 @@
 package moe.caffeine.fridgehero
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import moe.caffeine.fridgehero.model.FoodItem
 import moe.caffeine.fridgehero.repo.MongoRealm
 
 class MainViewModel : ViewModel() {
     val realm = MongoRealm
+
+    val foodItems = realm
+        .fetchAllByTypeAsFlow<FoodItem>()
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(),
+            emptyList()
+        )
 }
