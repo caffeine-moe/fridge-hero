25eecd9963fdc44cea92a6727941de8e4b8b8f60
caffeine01 <caffeine@caffeine.moe>
2024-11-01 03:40:23 +0000

    remove unneeded field from last change

--- app/src/main/java/moe/caffeine/fridgehero/model/FoodItem.kt
+++ app/src/main/java/moe/caffeine/fridgehero/model/FoodItem.kt
@@ -1,7 +1,5 @@
 package moe.caffeine.fridgehero.model
 
-import io.realm.kotlin.ext.backlinks
-import io.realm.kotlin.query.RealmResults
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 import org.mongodb.kbson.BsonObjectId
@@ -14,5 +12,4 @@ class FoodItem : RealmObject {
     var brand: String = ""
     var barcode: String = ""
     var expiryDate: Long = 0
-    val recipes: RealmResults<Recipe> by backlinks(Recipe::ingredients)
 }
