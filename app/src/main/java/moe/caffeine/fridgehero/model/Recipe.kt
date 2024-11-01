53a888280a703557616b221d95e26ec72616f815
caffeine01 <caffeine@caffeine.moe>
2024-11-01 03:20:53 +0000

    add Recipe object

--- app/src/main/java/moe/caffeine/fridgehero/model/Recipe.kt
+++ app/src/main/java/moe/caffeine/fridgehero/model/Recipe.kt
@@ -1,18 +1,16 @@
 package moe.caffeine.fridgehero.model
 
-import io.realm.kotlin.ext.realmListOf
-import io.realm.kotlin.types.RealmList
+import io.realm.kotlin.ext.backlinks
+import io.realm.kotlin.query.RealmResults
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 import org.mongodb.kbson.BsonObjectId
 
-class FoodItem : RealmObject {
+class Recipe : RealmObject {
     @PrimaryKey
     var _id: BsonObjectId = BsonObjectId()
 
     var name: String = ""
-    var brand: String = ""
-    var barcode: String = ""
-    var expiryDate: Long = 0
-    var recipes: RealmList<Recipe> = realmListOf()
+    val ingredients: RealmResults<FoodItem> by backlinks(FoodItem::recipes)
+    var instructions: String = ""
 }
