82a143f41053500b88cbc33a1518327962bfd557
caffeine01 <caffeine@caffeine.moe>
2024-11-01 01:29:48 +0000

    ignore fullname field in profile

--- app/src/main/java/moe/caffeine/fridgehero/model/Profile.kt
+++ app/src/main/java/moe/caffeine/fridgehero/model/Profile.kt
@@ -1,14 +1,18 @@
 package moe.caffeine.fridgehero.model
 
 import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.Ignore
 import io.realm.kotlin.types.annotations.PrimaryKey
 import org.mongodb.kbson.ObjectId
 
 class Profile : RealmObject {
     @PrimaryKey
-    var _id : ObjectId = ObjectId()
-    var firstName : String = ""
-    var lastName : String = ""
-    val fullName : String
+    var _id: ObjectId = ObjectId()
+
+    var firstName: String = ""
+    var lastName: String = ""
+
+    @Ignore
+    val fullName: String
         get() = "$firstName $lastName"
 }
