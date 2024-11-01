4478ed418f68fda492bcb138bb9682d10cf93b7e
caffeine01 <caffeine@caffeine.moe>
2024-11-01 01:47:02 +0000

    create initial FoodItem object

---
+++ app/src/main/java/moe/caffeine/fridgehero/model/FoodItem.kt
@@ -0,0 +1,15 @@
+package moe.caffeine.fridgehero.model
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.BsonObjectId
+
+class FoodItem : RealmObject {
+    @PrimaryKey
+    var _id: BsonObjectId = BsonObjectId()
+
+    var name: String = ""
+    var brand: String = ""
+    var barcode: String? = null
+    var expiryDate: Long = -1L
+}
