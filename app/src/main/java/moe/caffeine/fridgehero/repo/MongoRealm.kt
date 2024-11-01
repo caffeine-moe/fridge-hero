4e49eec046d129f087ee2626be193a5f2bad8286
caffeine01 <caffeine@caffeine.moe>
2024-11-01 15:15:38 +0000

    fetch object by id

--- app/src/main/java/moe/caffeine/fridgehero/repo/MongoRealm.kt
+++ app/src/main/java/moe/caffeine/fridgehero/repo/MongoRealm.kt
@@ -2,27 +2,36 @@ package moe.caffeine.fridgehero.repo
 
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.UpdatePolicy
 import io.realm.kotlin.ext.query
 import io.realm.kotlin.types.RealmObject
+import moe.caffeine.fridgehero.model.FoodItem
 import moe.caffeine.fridgehero.model.Profile
+import moe.caffeine.fridgehero.model.Recipe
+import org.mongodb.kbson.BsonObjectId
 
 object MongoRealm {
     val realm: Realm = Realm.open(
         configuration = RealmConfiguration.create(
             schema = setOf(
-                Profile::class
+                Profile::class,
+                FoodItem::class,
+                Recipe::class
             )
         )
     )
 
     fun updateObject(realmObject: RealmObject) {
         realm.writeBlocking {
-            copyToRealm(realmObject)
+            copyToRealm(realmObject, UpdatePolicy.ALL)
         }
     }
 
     inline fun <reified T : RealmObject> fetchAllByType(): List<T> =
         realm.query<T>().find().toList()
+
+    inline fun <reified T : RealmObject> fetchObjectById(id: BsonObjectId): T? =
+        realm.query<T>("_id == $0", id).first().find()
 
     fun deleteObject(realmObject: RealmObject) {
         realm.writeBlocking {
