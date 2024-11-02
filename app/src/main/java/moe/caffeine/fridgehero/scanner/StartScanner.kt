c227b1c94c171ebf178f256089c82b4bfb82821b
caffeine01 <caffeine@caffeine.moe>
2024-11-02 03:32:28 +0000

    add camera preview and analyser

--- app/src/main/java/moe/caffeine/fridgehero/scanner/StartScanner.kt
+++ app/src/main/java/moe/caffeine/fridgehero/scanner/StartScanner.kt
@@ -2,21 +2,32 @@ package moe.caffeine.fridgehero.scanner
 
 import android.Manifest
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun StartScanner(
-    barcode: (String) -> Unit
+    onBarcodeDetected: (String) -> Unit
 ) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
     val cameraPermissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
     LaunchedEffect(cameraPermissionState) {
         when {
@@ -33,6 +44,34 @@ fun StartScanner(
         val cameraSelector: CameraSelector = CameraSelector.Builder()
             .requireLensFacing(CameraSelector.LENS_FACING_BACK)
             .build()
-        println(cameraSelector.physicalCameraId)
+        val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+        val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+            ProcessCameraProvider.getInstance(context)
+
+        cameraProviderFuture.addListener({
+            val preview = Preview.Builder().build().also {
+                it.surfaceProvider = previewView.surfaceProvider
+            }
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val barcodeAnalyser = BarcodeAnalyser { barcode ->
+                onBarcodeDetected(barcode)
+                cameraProvider.unbindAll()
+                previewView.removeAllViews()
+            }
+            val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, barcodeAnalyser)
+                }
+
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(
+                lifecycleOwner,
+                cameraSelector,
+                preview,
+                imageAnalysis
+            )
+        }, ContextCompat.getMainExecutor(context))
     }
 }
