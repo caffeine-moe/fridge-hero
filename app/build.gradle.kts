4e1d850d47563ee8186199449b2602d7040b31dd
caffeine01 <caffeine@caffeine.moe>
2024-11-03 19:42:25 +0000

    add json serialisation plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("io.realm.kotlin")
+    kotlin("plugin.serialization") version "2.0.20"
 }
 
 android {
