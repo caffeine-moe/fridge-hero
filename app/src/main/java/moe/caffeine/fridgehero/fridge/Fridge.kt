ae1d8c2eb0cc1ad1e971729262192dd7220a39c5
caffeine01 <caffeine@caffeine.moe>
2024-11-03 21:52:33 +0000

    extract barcode handle logic

--- app/src/main/java/moe/caffeine/fridgehero/fridge/Fridge.kt
+++ app/src/main/java/moe/caffeine/fridgehero/fridge/Fridge.kt
@@ -1,6 +1,5 @@
 package moe.caffeine.fridgehero.fridge
 
-import android.widget.Toast
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
@@ -8,7 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import moe.caffeine.fridgehero.MainViewModel
@@ -18,7 +17,7 @@ import moe.caffeine.fridgehero.scanner.StartScanner
 @Composable
 fun Fridge(viewModel: MainViewModel) {
     val fridge by viewModel.foodItems.collectAsState()
-    var showScanner by remember { mutableStateOf(false) }
+    var showScanner by rememberSaveable { mutableStateOf(false) }
     val context = LocalContext.current
     Scaffold(
         floatingActionButton = {
@@ -44,38 +43,19 @@ fun Fridge(viewModel: MainViewModel) {
                         ItemCard(foodItem.name)
                     },
                     onDismiss = {
-                        viewModel.removeFromRealm(foodItem)
+                        viewModel.removeFromRealm(foodItem, 450)
                     }
                 )
             }
         }
     }
-    if (showScanner) {
-        StartScanner { barcode ->
-            showScanner = false
-            Toast.makeText(
-                context,
-                "Processing barcode $barcode, please wait...",
-                Toast.LENGTH_SHORT
-            ).show()
-            viewModel.createFoodItemFromBarcode(barcode) { result ->
-                result.fold(
-                    onFailure = { throwable ->
-                        Toast.makeText(
-                            context,
-                            "ERROR: ${throwable.message}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    },
-                    onSuccess = { foodItem ->
-                        Toast.makeText(
-                            context,
-                            "Successfully scanned ${foodItem.name}!",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        viewModel.addToRealm(foodItem)
-                    }
-                )
+    when {
+        showScanner -> {
+            StartScanner { barcode ->
+                showScanner = false
+                handleBarcode(barcode, viewModel, context) {
+                    
+                }
             }
         }
     }
