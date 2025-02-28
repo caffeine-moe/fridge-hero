5f160616f90345d6fce2f78e8bd0d81b3720939e
caffeine01 <caffeine@caffeine.moe>
2025-02-28 23:32:45 +0000

    fix permanent delete button logic

--- app/src/main/java/moe/caffeine/fridgehero/data/mapper/OpenFoodFactsMapper.kt
+++ app/src/main/java/moe/caffeine/fridgehero/data/mapper/OpenFoodFactsMapper.kt
@@ -3,14 +3,13 @@ package moe.caffeine.fridgehero.data.mapper
 import moe.caffeine.fridgehero.data.model.openfoodfacts.OpenFoodFactsProduct
 import moe.caffeine.fridgehero.domain.model.FoodCategory
 import moe.caffeine.fridgehero.domain.model.FoodItem
-import org.mongodb.kbson.BsonObjectId
 
 fun OpenFoodFactsProduct.toDomainModel(
   thumbnail: ByteArray,
   categories: List<FoodCategory>
 ): FoodItem =
   FoodItem(
-    realmObjectId = BsonObjectId().toHexString(),
+    realmObjectId = "",
     name = productName,
     barcode = code,
     brand = brands,
